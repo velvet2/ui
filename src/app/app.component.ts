c908d4e95e63f77a581f4d89d893fee93894a71e
Lee Yeong Khang <sindrosa.24@gmail.com>
2017-03-28 22:56:52 +0800

    add sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    menus: Array<any> = [
+        { "icon" : "person", "menu": "User", "link" : "/user" }
+    ]
 }
